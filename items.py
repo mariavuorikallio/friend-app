ab19a1879e63986d8d496f6513f51f91e02130ec
Vuorikallio Maria A A <vuomaria@cubbli24-gold.cs.helsinki.fi>
2025-09-28 19:24:18 +0300

    Add classes to edit page

--- items.py
+++ items.py
@@ -43,9 +43,16 @@ def get_item(item_id):
     result = db.query(sql, [item_id])
     return result[0] if result else None
      
-def update_item(item_id, title, description):
+def update_item(item_id, title, description, classes):
     sql = """UPDATE items SET title = ?, description = ? WHERE id = ?"""
-    db.execute(sql, [title, description, item_id])    
+    db.execute(sql, [title, description, item_id])
+    
+    sql = "DELETE FROM item_classes WHERE item_id = ?"
+    db.execute(sql, [item_id])
+    
+    sql = "INSERT into item_classes (item_id, title, value) VALUES (?, ?, ?)"
+    for title, value in classes:
+        db.execute(sql, [item_id, title, value])    
     
 def remove_item(item_id):
     sql = "DELETE FROM items WHERE id = ?"
