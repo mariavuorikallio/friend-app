b7a64b96c0ec0c9542ae2ed0f0b5b4936d71c5e8
Vuorikallio Maria A A <vuomaria@cubbli24-gold.cs.helsinki.fi>
2025-10-09 19:41:19 +0300

    Update database schema

--- schema.sql
+++ schema.sql
@@ -1,28 +1,50 @@
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    password_hash TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    image BLOB
 );
 
 CREATE TABLE classes (
-   id INTEGER PRIMARY KEY,
-   title TEXT,
-   value TEXT
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   title TEXT NOT NULL,
+   value TEXT NOT NULL
 );
 
 CREATE TABLE messages (
-   id INTEGER PRIMARY KEY,
-   title TEXT,
-   description TEXT,
-   age INTEGER,
-   user_id INTEGER REFERENCES users
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   title TEXT NOT NULL,
+   description TEXT NOT NULL,
+   age INTEGER NOT NULL,
+   user_id INTEGER NOT NULL,
+   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE message_classes (
-    id INTEGER PRIMARY KEY,
-    message_id INTEGER REFERENCES messages,
-    title TEXT,
-    value TEXT    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    message_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    value TEXT NOT NULL,
+    FOREIGN KEY(message_id) REFERENCES messages(id)
+);
+  
+CREATE TABLE threads (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ad_id INTEGER NOT NULL,
+    user1_id INTEGER NOT NULL,
+    user2_id INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(ad_id) REFERENCES messages(id),
+    FOREIGN KEY(user1_id) REFERENCES users(id),
+    FOREIGN KEY(user2_id) REFERENCES users(id)
 );
 
-  
+CREATE TABLE thread_messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    thread_id INTEGER NOT NULL,
+    sender_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(thread_id) REFERENCES threads(id),
+    FOREIGN KEY(sender_id) REFERENCES users(id)
+);
